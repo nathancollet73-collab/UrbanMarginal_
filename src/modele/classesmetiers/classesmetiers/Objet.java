26a16e23a40f2e70fe1cc4aad144846c1c757c87
Nathan <nathancollet73@gmail.com>
2026-04-09 18:01:37 +0200

    Phase 2 : MVC + ajout d'outils

---
+++ src/modele/classesmetiers/classesmetiers/Objet.java
@@ -0,0 +1,28 @@
+package modele;
+
+/**
+ * Informations communes � tous les objets (joueurs, murs, boules)
+ * permet de m�moriser la position de l'objet et de g�rer les  collisions
+ *
+ */
+public abstract class Objet {
+
+	/**
+	 * position X de l'objet
+	 */
+	protected Integer posX ;
+	/**
+	 * position Y de l'objet
+	 */
+	protected Integer posY ;
+	
+	/**
+	 * contr�le si l'objet actuel touche l'objet pass� en param�tre
+	 * @param objet contient l'objet � contr�ler
+	 * @return true si les 2 objets se touchent
+	 */
+	public Boolean toucheObjet (Objet objet) {
+		return null;
+	}
+	
+}
